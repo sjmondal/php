ffd66d4ff845f9a55c38d8e08144eb465724c0f2
sjmondal <shamsusjaman@gmail.com>
2017-11-30 21:40:16 +0530

    test push

---
+++ aa.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+//unset($_SESSION['stored']);
+if(isset($_POST['submit'])){
+$myfile = fopen("bb.php", "a") or die("Unable to open file!");
+$stored_ar = array();
+$input_ar = array('name'=>$_POST['name'],'age'=>$_POST['age']);
+$current_final_ar = array();
+if(isset($_SESSION['stored'])){
+	$stored_ar = $_SESSION['stored'];
+	$temp = $stored_ar;
+	//$current_final_ar = $temp;
+	$current_final_ar = $input_ar;
+	$_SESSION['stored'][] = $current_final_ar;
+}else
+{
+$current_final_ar[] = $input_ar;
+$_SESSION['stored'] = 	$current_final_ar;
+}
+/*echo '<pre>';
+print_r($_SESSION['stored']);
+echo '</pre>';*/
+//$txt = "";
+/*fwrite($myfile, $txt);
+$txt = "\n";
+fwrite($myfile, $txt);
+fclose($myfile);*/
+
+if(isset($_SESSION['stored']) && !empty($_SESSION['stored'])){
+	header("Location:bb.php");
+}
+}
+
+
+?>
+<div class="formDiv">
+<form action='' method="post">
+<div>
+<div class="each_sec">Name<br>
+<input type="text" name="name">
+</div>
+<div class="each_sec">Age<br>
+<input type="text" name="age">
+</div>
+</div>
+<div class="btnSubmit" style="clear:both"><input type="submit" name="submit" value="Submit"></div>
+</form>
+</div>
+<style>
+.each_sec{float:left;width:200px;margin:0 10px 0 0;}
+.formDiv{width:60%;border:1px solid #000;padding:30px;height:200px;}
+.btnSubmit input {
+    margin: 10px 52px 0 0;
+    float: right;
+}
+</style>
---
+++ bb.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+/*echo '<pre>';
+print_r($_SESSION['stored']);
+echo '</pre>';*/
+if(isset($_POST['action'])){
+	$ses_ar = $_SESSION['stored'];
+	$sort = array();
+foreach($ses_ar as $k=>$v) {
+    $sort['name'][$k] = $v['name'];
+    $sort['age'][$k] = $v['age'];
+}
+	if($_POST['sort_by']=='age' && $_POST['sort_dir']=='asc'){
+	//sort($_SESSION['stored']);
+	//$_SESSION['stored'] = $_SESSION['stored'];
+	array_multisort($sort['age'], SORT_ASC,$sort['age'], SORT_ASC,$ses_ar);
+	}
+	elseif($_POST['sort_by']=='age' && $_POST['sort_dir']=='desc'){
+	//sort($_SESSION['stored']);
+	//$_SESSION['stored'] = $_SESSION['stored'];
+	array_multisort($sort['age'], SORT_DESC,$sort['age'], SORT_DESC,$ses_ar);
+	}
+	
+	elseif($_POST['sort_by']=='name' && $_POST['sort_dir']=='asc'){
+	array_multisort($sort['name'], SORT_ASC,$sort['name'], SORT_ASC,$ses_ar);
+	}
+	elseif($_POST['sort_by']=='name' && $_POST['sort_dir']=='desc'){
+	array_multisort($sort['name'], SORT_DESC,$sort['name'], SORT_DESC,$ses_ar);
+	}
+	
+$_SESSION['stored'] = $ses_ar;
+}
+
+if(isset($_SESSION['stored']) && !empty($_SESSION['stored'])){
+	?>
+     <form id="listform" name="list" action="" method="post">
+    <table>
+    <tr><th align="center" width="50%">Name<a href="javascript:void(0);" onClick="submitForm('name','asc');">
+    <i class="fa fa-arrow-down" aria-hidden="true"></i>
+    </a>
+    <a href="javascript:void(0);" onClick="submitForm('name','desc');"><i class="fa fa-arrow-up" aria-hidden="true"></i></a></th><th width="50%">Age<a href="javascript:void(0);" onClick="submitForm('age','asc');"><i class="fa fa-arrow-down" aria-hidden="true"></i></a>
+    <a href="javascript:void(0);" onClick="submitForm('age','desc');"><i class="fa fa-arrow-up" aria-hidden="true"></i></a></th></tr>
+	<?php
+	foreach($_SESSION['stored'] as $index=>$eachVal)
+	{
+	?>
+<tr><td align="center" width="50%"><?php echo $eachVal['name'];?></td><td><?php echo $eachVal['age'];?></td></tr>
+<?php
+	}
+	?>
+    </table>
+    <input type="button" onclick="window.location.href='aa.php'" value="Back">
+    <input type="hidden" id="sort_by" name="sort_by" value="">
+    <input type="hidden" id="sort_dir" name="sort_dir" value="">
+    <input type="hidden" name="action" value="1">
+    </form>
+    <?php
+}
+?>
+<script>
+function submitForm(sort_by,sort_dir)
+{
+document.getElementById("sort_by").value = sort_by;
+document.getElementById("sort_dir").value = sort_dir;
+document.getElementById("listform").submit();
+}
+</script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
