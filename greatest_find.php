111c0446efd324b476996536230ea7d160af6750
sjmondal <shamsusjaman@gmail.com>
2017-12-08 18:51:50 +0530

    Find the greatest element of an array

---
+++ greatest_find.php
@@ -0,0 +1,22 @@
+<title>Find greatest from array</title>
+<?php
+$my_array = array(16,22,50,6,1,1,9,20,4,7,12,3,15,20,43);
+
+function array_max($arr){
+	$temp = '';
+foreach($arr as $index=>$elem){
+	if($temp==''){
+		if($elem>=$my_array[($index+1)])
+			$temp = $elem;
+		else $temp = $my_array[($index+1)];
+		}
+		else{
+		if($elem>=$temp)
+			$temp = $elem;
+		else $temp = $temp;	
+		}
+}
+return  $temp;
+}
+echo array_max($my_array);
+?>
